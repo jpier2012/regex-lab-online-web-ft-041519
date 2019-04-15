93c2fdc109f95c10ac7162c47831eaacb4f20b07
James Pier <jpier2012@gmail.com>
2019-04-15 20:16:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,9 +8,14 @@ def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w*ing/i)
 end
 
+test_text = "extreme briny crepe parking snaps grouping snafu round dog be fork spoon"
+
+
 def words_five_letters_long(text)
-  text.scan(/\w{5}(?=\s)/)
+  text.scan(/\b\w{5}\b/)
 end
+
+words_five_letters_long()
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 

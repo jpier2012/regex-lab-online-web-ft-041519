7415cf38196baaad697e9e4c4c9de35879f9ad24
James Pier <jpier2012@gmail.com>
2019-04-15 20:09:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,11 +5,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  new_array = text.scan(/un\w*ing/)
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)

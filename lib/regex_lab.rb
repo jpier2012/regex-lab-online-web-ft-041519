a947602371a10339b493c1b1226ffca028569db9
James Pier <jpier2012@gmail.com>
2019-04-15 20:05:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   new_array = text.scan(/un.*ing/)
 end
 
-words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
+puts words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
 
 
 def words_five_letters_long(text)

3b88e6391977b607f83440529c1498a21dbee166
James Pier <jpier2012@gmail.com>
2019-04-15 19:45:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ def starts_with_a_vowel?(word)
   word[0].match(/[aeiou]/) ? true : false
 end
 
-puts starts_with_a_vowel?("apple")
+#puts starts_with_a_vowel?("apple")
 
 def words_starting_with_un_and_ending_with_ing(text)
 

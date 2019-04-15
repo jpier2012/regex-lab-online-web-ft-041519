eb559d7d48d2f046f17bec030ae2d6d14af0872a
James Pier <jpier2012@gmail.com>
2019-04-15 20:07:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  new_array = text.scan(/un.*ing(?=\s))/)
+  new_array = text.scan(/un.*ing(?=\s)/)
 end
 
 puts words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
